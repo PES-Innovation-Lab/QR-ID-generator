db82b6b187f890f4c04621cda69042fc228a0c57
hat-im <hatim3121@gmail.com>
2021-10-22 23:20:08 +0530

    stupid mistake

--- qr_gen.py
+++ qr_gen.py
@@ -2,7 +2,6 @@ import qrcode
 import uuid
 import os
 import sys
-
 # 120 ID cards by default
 if len(sys.argv)<2: n = 170
 else: n = int(sys.argv[1])
@@ -13,4 +12,4 @@ f=open(os.path.join("QR Code","data.csv"),"w")
 for i in range(n):
     id = uuid.uuid4().hex
     qrcode.make(id).save(os.path.join("QR Code",f"{i}.png"))
-    print(f"false,false,false,false,{uuid.uuid4().hex},false,false,,", file=f)
+    print(f"false,false,false,false,{id},false,false,,", file=f)
